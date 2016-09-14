6dd098c234f4e09bb10dc1db8f282c57a9941c10
katjam <katja@neontribe.co.uk>
2016-09-14 14:35:00 +0100

    Add wcr copyright to footer

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 add_action( 'wp_enqueue_scripts', 'enqueue_compiled_styles');
+add_action( 'wcr_footer_copyright', 'wcr_footer_copyright', 10 );
 
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
@@ -19,4 +20,11 @@ function enqueue_compiled_styles() {
   );
   // Could also use conditional 'lt IE 9' or 'IE 7' etc.
   wp_style_add_data( 'wcr-sass-ie', 'conditional', 'IE');
+}
+
+function wcr_footer_copyright() {
+  $copyright_txt = 'Copyright &copy; '.date('Y').' West Country Rural Ltd.';
+  $theme_info = ' Powered by Wordpress. Theme based on Spacious by ThemeGrill.';
+  $copyright_info = '<div class="copyright">'.$copyright_txt.$theme_info.'</div>';
+  echo $copyright_info;
 }
