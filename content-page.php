4b66db785c94fad23a72dc56836ccd02c3918337
katjam <katja@neontribe.co.uk>
2016-09-18 19:52:14 +0100

    remove page image links

--- content-page.php
+++ content-page.php
@@ -21,8 +21,7 @@ if( has_post_thumbnail() ) {
     $image = '';
     $title_attribute = get_the_title( $post->ID );
     $image .= '<figure class="post-featured-image">';
-    $image .= '<a href="' . get_permalink() . '" title="'.the_title( '', '', false ).'">';
-    $image .= get_the_post_thumbnail( $post->ID, $featured, array( 'title' => esc_attr( $title_attribute ), 'alt' => esc_attr( $title_attribute ) ) ).'</a>';
+    $image .= get_the_post_thumbnail( $post->ID, $featured, array( 'title' => esc_attr( $title_attribute ), 'alt' => esc_attr( $title_attribute ) ) );
     $image .= '</figure>';
 
     echo $image;
